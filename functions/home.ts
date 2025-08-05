0c9bf5464b370e1f24feca1adacc5ede7fc21849
huilang-me <76546468@qq.com>
2025-08-05 15:06:54 +0800

    Update home.ts

--- functions/home.ts
+++ functions/home.ts
@@ -164,7 +164,9 @@ export async function renderHomePage(env): Promise<Response> {
                 const hasFail = statuses.some(s => s !== 'up');
                 const cls = statuses.length === 0 ? '' : hasFail ? 'fail' : 'ok';
                 const title = key + (statuses.length === 0 ? ': 无数据' : hasFail ? ': 异常' : ': 正常');
-                bars.push('<div class=&quot;bar ' + cls + '&quot; title=&quot;' + title + '&quot; onclick=&quot;showPopup(\\\'' + key + '\\\', \\\'' + name + '\\\')&quot;></div>');
+                //bars.push('<div class="bar ' + cls + '" title="' + title + '" onclick="showPopup(\\'' + key + '\\', \\' + siteName + '\\')"></div>');
+                bars.push('<div class="bar ' + cls + '" title="' + title + '" onclick="showPopup(\\'' + key + '\\')"></div>');
+
               }
 
               const container = document.getElementById('bar-' + name.replace(/[^a-zA-Z0-9]/g, ""));
